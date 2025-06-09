d0d72a7c7b3f2f7e2a1312fba43ac6bd3b2a0fc9
Pedro Lucena <pedrocontact22@gmail.com>
2025-06-09 02:05:30 -0300

    🔧 chore: Add more mock data

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,12 +1,14 @@
 import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc'
 
+import { usersRouter } from './routers/users'
+
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  // post: postRouter,
+  users: usersRouter,
 })
 
 // export type definition of API
