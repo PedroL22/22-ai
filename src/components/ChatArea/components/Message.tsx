7c95f1d5ebf6e00cb8c2c70f78dfd9028e840b8b
Pedro Lucena <pedrocontact22@gmail.com>
2025-06-15 06:31:19 -0300

    ✨ feat: Add refresh button to message component

--- src/components/ChatArea/components/Message.tsx
+++ src/components/ChatArea/components/Message.tsx
@@ -2,25 +2,32 @@
 
 import { cva } from 'class-variance-authority'
 import { AnimatePresence, motion } from 'motion/react'
+import Image from 'next/image'
 import { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
-import { Check, Copy } from 'lucide-react'
+import { Check, Copy, Info, RefreshCcw, Sparkles } from 'lucide-react'
 import { toast } from 'sonner'
 import { Button } from '~/components/ui/button'
-import {} from '~/components/ui/tooltip'
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '~/components/ui/dropdown-menu'
+import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip'
 import { formatMessageDateForChatHistory } from '~/utils/format-date-for-chat-history'
 import { getModelName } from '~/utils/get-model-name'
 
 import type { Message as MessageType } from '@prisma/client'
-import type { ModelsIds } from '~/types/models'
+import { MODELS, type ModelsDevelopers, type ModelsIds } from '~/types/models'
 
 const messageVariants = cva('group relative flex flex-col gap-1 rounded-2xl px-4 py-3 text-sm', {
   variants: {
     variant: {
-      user: 'max-w-[70%] self-end bg-border/80 bg-primary dark:bg-zinc-700/80',
+      user: 'max-w-[70%] self-end bg-border/80 bg-primary',
       assistant: 'max-w-full self-start bg-transparent',
     },
   },
@@ -36,9 +43,21 @@ type MessageProps = {
 export const Message = ({ message }: MessageProps) => {
   const [isCopied, setIsCopied] = useState(false)
 
+  const developerIcon = (developer: ModelsDevelopers) => {
+    switch (developer) {
+      case 'Google':
+        return <Image src='/images/icons/gemini.svg' alt='Gemini Logo' width={16} height={16} className='size-4' />
+      case 'DeepSeek':
+        return <Image src='/images/icons/deepseek.svg' alt='DeepSeek Logo' width={16} height={16} className='size-4' />
+
+      default:
+        return <Sparkles className='size-4 text-zinc-400' />
+    }
+  }
+
   return (
     <div className={messageVariants({ variant: message.role })}>
-      <div className='max-w-none whitespace-pre-wrap break-words'>
+      <div data-role={message.role} className='max-w-none whitespace-pre-wrap break-words data-[role=user]:text-white'>
         <ReactMarkdown
           remarkPlugins={[remarkGfm]}
           components={{
@@ -51,9 +70,55 @@ export const Message = ({ message }: MessageProps) => {
 
       <div
         data-role={message.role}
-        className='data-[role=user]:-bottom-9 -bottom-8 absolute flex flex-row-reverse items-center gap-1 self-start whitespace-nowrap text-muted-foreground opacity-0 transition-all ease-in group-hover:opacity-100 data-[role=user]:right-0 data-[role=assistant]:left-3 data-[role=user]:flex-row data-[role=user]:self-end data-[role=user]:text-zinc-300'
+        className='data-[role=user]:-bottom-9 -bottom-8 absolute flex flex-row-reverse items-center gap-1 self-start whitespace-nowrap text-muted-foreground opacity-0 transition-all ease-in group-hover:opacity-100 data-[role=user]:right-0 data-[role=assistant]:left-3 data-[role=user]:flex-row data-[role=user]:self-end dark:data-[role=user]:text-zinc-300'
       >
-        <p className='text-xs'>{`${message.modelId ? `${getModelName(message.modelId as ModelsIds)} ` : ''}${formatMessageDateForChatHistory(message.createdAt.toISOString())}`}</p>
+        <p className='px-1 text-xs'>{`${message.modelId ? `${getModelName(message.modelId as ModelsIds)} ` : ''}${formatMessageDateForChatHistory(message.createdAt.toISOString())}`}</p>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant='ghost'
+              title='Retry message'
+              data-role={message.role}
+              className='aspect-square size-8 rounded-sm hover:bg-accent-foreground/5 dark:hover:bg-accent-foreground/5'
+            >
+              <RefreshCcw className='size-4' />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            <DropdownMenuItem className='flex items-center space-x-0.5 px-3 py-2 text-muted-foreground text-xs transition-all ease-in'>
+              <RefreshCcw className='size-3' /> <span className='font-medium'>Retry same</span>
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator />
+
+            {MODELS.map((model) => (
+              <DropdownMenuItem
+                key={model.id}
+                className='flex items-center justify-between space-y-1 px-3 py-2 transition-all ease-in'
+              >
+                <div className='flex w-full items-center space-x-2'>
+                  {developerIcon(model.developer)}
+
+                  <div className='flex w-full items-center justify-between space-x-4'>
+                    <span className='font-medium text-muted-foreground text-xs'>{model.name}</span>
+
+                    <Tooltip>
+                      <TooltipTrigger className='cursor-pointer '>
+                        <Info className='size-3' />
+                      </TooltipTrigger>
+
+                      <TooltipContent>
+                        <p className='max-w-[300px]'>{model.description}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </div>
+                </div>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
 
         <Button
           variant='ghost'
